2870a58223fbbccc5a3c9a3bd81ecf32e5c9de32
MatheusBusarello <matheus.b.almeida@outlook.com>
2025-02-25 21:42:21 -0300

    Construção das páginas de artista e música

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const SongItem = () => {
+const SongItem = ({ image, name, duration, artist, audio, id, index }) => {
   return (
-    <Link to="/song/1" className="song-item">
+    <Link to={`/song/${id}`} className="song-item">
       <div className="song-item__number-album">
-        <p>1</p>
+        <p>{index + 1}</p>
 
         <div className="song-item__album">
           <img 
-            src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24" 
-            alt="Song image"
+            src={image} 
+            alt={`Song ${name} image`}
             className="song-item__image"
           />
 
-          <p className="song-item__name">Ultima Saudade - Ao vivo</p>
+          <p className="song-item__name">{name}</p>
         </div>
       </div>
        
-      <p>02:30</p>
+      <p>{duration}</p>
     </Link>
   )
 }
